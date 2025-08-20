f946aeb4e6c7a3c5a74ec42526a32ea02620770f
muro906 <mainamillicent68@gmail.com>
2025-08-20 22:30:34 +0300

    Spring project

---
+++ lendingservice/src/main/java/com/h20/lendingservice/LendingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.h20.lendingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendingserviceApplication.class, args);
+	}
+
+}
---
+++ lendingservice/src/test/java/com/h20/lendingservice/LendingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h20.lendingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
